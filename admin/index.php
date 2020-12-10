c738d21719872b539e96ed76a44e411eea6b5025
AgostinhoDossantos <agostinhodossantos27@gmail.com>
2020-12-10 01:42:46 -0800

    publish page

--- admin/index.php
+++ admin/index.php
@@ -175,6 +175,22 @@ foreach($fetchdata as $key => $row){
         </div>
       </li>
 
+
+        <!-- Nav Item - Utilities Collapse Menu -->
+        <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
+          <i class="fas fa-fw fa-rss-square"></i>
+          <span>Publicações </span>
+        </a>
+        <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Cursos:</h6>
+            <a class="collapse-item" href="add_course.php?id=<?php echo $uid; ?>">Publicar</a>
+            <a class="collapse-item" href="courses.php?id=<?php echo $uid; ?>">Ver publicações </a>
+          </div>
+        </div>
+      </li>
+
       <!-- Divider -->
       <hr class="sidebar-divider">
 
